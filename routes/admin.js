ddc7af6a622006d684cfbcaa281a2585cb272264
Opilane TTHK <opilane@tthk.ee>
2020-03-03 09:31:30 +0200

    wwww

---
+++ routes/admin.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const rootDirectory = require('../utilities/path');
+const products = [];
+
+//to use the code from this file in server.js
+const router = express.Router();
+
+router.get('/add-product', function(req, res){
+    res.render('add-product', {
+        pageTitle: "Add New Product",
+        path: '/admin/add-product'
+    })
+    //res.sendFile(path.join(rootDirectory, 'views', 'add-product.html'));
+});
+
+router.post('/add-product', function(req, res){
+    console.log(req.body.title);
+    products.push({
+        title: req.body.title,
+        //price: req.body.price,
+        //description: req.body.desc
+    });
+    res.redirect('/');
+});
+
+exports.products = products;
+exports.router = router;
+
+//module.exports = router;
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+//userRoutes - injecting code from shop.js
+const userRoutes = require('./routes/shop');
+//adminData - injecting code from admin.js
+const adminData = require('./routes/admin');
+const path = require('path');
+
+const app = express();
+app.set('view engine', 'ejs');  
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/admin', adminData.router);
+app.use(userRoutes);
+
+
+
+app.use(function(req, res, next){
+    res.status(404).render('404', {
+        pageTitle: "Oops! Page Not Found!",
+        pageNotFound: "No Luck. Try Again.",
+        myVariable: "Hello World!",
+        path: ''
+    
+    });
+
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+
+    //res.status(404).send("Not found");
+});
+
+
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+    //console.log("Path", path);
+});
+
+
